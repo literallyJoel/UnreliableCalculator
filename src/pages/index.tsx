f2dace6541473cad4a9f68061a12f3c536588db3
Joel Vivian <joel@jdvivian.co.uk>
2024-03-03 18:12:50 +0000

    typo fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -117,7 +117,7 @@ export default function Home() {
         <div className="flex flex-col gap-2">
           <div className="text-center text-2xl">
             Number of Calculations:{" "}
-            {(stats.data?.correct ??) 0 + (stats.data?.incorrect ?? 0)}
+            {(stats.data?.correct ?? 0) + (stats.data?.incorrect ?? 0)}
           </div>
           <div className="text-center text-3xl">
             Correct & Incorrect Answers
@@ -138,16 +138,9 @@ export default function Home() {
         <div className="flex flex-col gap-2">
           <div className="text-center text-3xl">Error Sizes</div>
           <div className="grid grid-cols-3 gap-4 text-center">
-            <div>
-              Largest Error: {stats.data?.largestOffset?.offset}
-            </div>
-            <div>
-              Smallest Error: {stats.data?.smallestOffset?.offset}
-            </div>
-            <div>
-              Average Error:{" "}
-              {stats.data?.averageOffset._avg.offset}
-            </div>
+            <div>Largest Error: {stats.data?.largestOffset?.offset}</div>
+            <div>Smallest Error: {stats.data?.smallestOffset?.offset}</div>
+            <div>Average Error: {stats.data?.averageOffset._avg.offset}</div>
           </div>
         </div>
       </div>
